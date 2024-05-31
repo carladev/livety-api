0eea08a4eb1d09b66ea912668588e710bd1e7d0f
carladev <carla@coderty.com>
2024-05-31 19:29:50 +0200

    wip

--- src/Routes/tracking-routes.php
+++ src/Routes/tracking-routes.php
@@ -10,33 +10,32 @@ return function (App $app, $jwtMiddleware) {
         $weekNumber = $args['weekNumber'];
         $userId = $request->getAttribute('userId');
 
-        $sql = "WITH HabitWeekDays AS (
-                  SELECT
-                      H.habitId,
-                      H.habitName,
-                      H.color,
-                      H.icon,
-                      H.habitGoal,
-                      WD.weekdayId,
-                      WD.weekdayName
-                 FROM LIV.habits H
-           CROSS JOIN LIV.weekDays WD
-                WHERE H.userId = :userId)
-              SELECT
-                  HWD.habitId,
-                  HWD.habitName,
-                  HWD.color,
-                  HWD.icon,
-                  HWD.habitGoal,
-                  HWD.weekdayId,
-                  HWD.weekdayName,
-                  ROUND(COALESCE((HR.record * 100)/HWD.habitGoal, 0), 0) AS progress,
-                  HR.recordDate
-             FROM HabitWeekDays HWD
-        LEFT JOIN LIV.habitRecords HR ON HWD.habitId = HR.habitId
-              AND WEEKDAY(HR.recordDate) = HWD.weekdayId
-              AND WEEK(HR.recordDate, 1) = :weekNumber
-         ORDER BY HWD.habitId, HWD.weekdayId, progress DESC";
+        $sql = "SELECT HWD.habitId,
+                       HWD.habitName,
+                       HWD.color,
+                       HWD.icon,
+                       HWD.habitGoal,
+                       HWD.weekdayId,
+                       HWD.weekdayName,
+                       ROUND(COALESCE((HR.record * 100) / HWD.habitGoal, 0), 0) AS progress,
+                       HR.recordDate
+                 FROM (
+                     SELECT
+                         H.habitId,
+                         H.habitName,
+                         H.color,
+                         H.icon,
+                         H.habitGoal,
+                         WD.weekdayId,
+                         WD.weekdayName
+                     FROM LIV.habits H
+                     CROSS JOIN LIV.weekDays WD
+                     WHERE H.userId = :userId
+                 ) HWD
+                 LEFT JOIN LIV.habitRecords HR ON HWD.habitId = HR.habitId
+                     AND WEEKDAY(HR.recordDate) = HWD.weekdayId
+                     AND WEEK(HR.recordDate, 1) = :weekNumber
+                 ORDER BY HWD.habitId, HWD.weekdayId, progress DESC";
 
         try {
             $db = new DB();
@@ -69,6 +68,92 @@ return function (App $app, $jwtMiddleware) {
             }
 
             $response->getBody()->write(json_encode(array_values($weekdaysHabits)));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(200);
+        } catch (PDOException $e) {
+            $error = array(
+                "message" => $e->getMessage()
+            );
+
+            $response->getBody()->write(json_encode($error));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(500);
+        }
+    })->add($jwtMiddleware);
+
+    $app->get('/api/monthly-tracking/{monthNumber}', function (Request $request, Response $response, $args) {
+        $monthNumber = $args['monthNumber'];
+        $userId = $request->getAttribute('userId');
+
+        $sql = "SELECT * FROM (
+            SELECT
+                HWD.habitId,
+                HWD.habitName,
+                HWD.color,
+                HWD.icon,
+                HWD.habitGoal,
+                HWD.monthdayNumber,
+                ROUND(COALESCE((HR.record * 100) / HWD.habitGoal, 0), 0) AS progress,
+                HR.recordDate
+            FROM (
+                SELECT
+                    H.habitId,
+                    H.habitName,
+                    H.color,
+                    H.icon,
+                    H.habitGoal,
+                    DM.dayNumber AS monthdayNumber
+                FROM LIV.habits H
+                CROSS JOIN LIV.daysOfMonth DM
+                WHERE H.userId = :userId
+            ) HWD
+            LEFT JOIN LIV.habitRecords HR ON HWD.habitId = HR.habitId
+                AND DAY(HR.recordDate) = HWD.monthdayNumber
+                AND MONTH(HR.recordDate) = :monthNumber
+                AND YEAR(HR.recordDate) = EXTRACT(YEAR FROM NOW())
+            ORDER BY HWD.habitId, HWD.monthdayNumber, progress DESC) HRF
+                     GROUP BY habitId,
+            habitName,
+            color,
+            icon,
+            habitGoal,
+            monthdayNumber,
+            progress,
+            recordDate
+            HAVING MAX(HRF.monthdayNumber) <= DAY(LAST_DAY(CONCAT(EXTRACT(YEAR FROM NOW()), '-', :monthNumber, '-01')))";
+
+        try {
+            $db = new DB();
+            $conn = $db->connect();
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':monthNumber', $monthNumber, PDO::PARAM_INT);
+            $stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
+            $stmt->execute();
+            $results = $stmt->fetchAll(PDO::FETCH_OBJ);
+            $db = null;
+
+            $monthdaysHabits = [];
+            foreach ($results as $row) {
+                if (!isset($monthdaysHabits[$row->monthdayNumber])) {
+                    $monthdaysHabits[$row->monthdayNumber] = [
+                        'monthdayNumber' => $row->monthdayNumber,
+                        'habits' => []
+                    ];
+                }
+                $monthdaysHabits[$row->monthdayNumber]['habits'][] = [
+                    'habitId' => $row->habitId,
+                    'habitName' => $row->habitName,
+                    'color' => $row->color,
+                    'icon' => $row->icon,
+                    'habitGoal' => $row->habitGoal,
+                    'progress' => $row->progress,
+                    'recordDate' => $row->recordDate
+                ];
+            }
+
+            $response->getBody()->write(json_encode(array_values($monthdaysHabits)));
             return $response
                 ->withHeader('content-type', 'application/json')
                 ->withStatus(200);
